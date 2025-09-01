b9ebd209df4a90f8d7435839c2a1ae0bc65e8d33
FlamingWater35 <flamingwater69420@gmail.com>
2025-09-01 17:58:20 +0300

    Update app version and Android sdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.flamingwater.quizlone"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = 24
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
