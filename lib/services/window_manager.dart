452ef7dcc132026567bc55a6cfa48ff1ef2eef74
FlamingWater35 <flamingwater69420@gmail.com>
2026-04-08 18:51:43 +0300

    Update window settings

--- lib/services/window_manager.dart
+++ lib/services/window_manager.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:quizlone/i18n/generated/translations.g.dart';
@@ -13,7 +14,12 @@ void setupWindow() async {
   if (_isWindows) {
     await windowManager.ensureInitialized();
 
-    WindowOptions windowOptions = WindowOptions(center: true, title: t.appName);
+    WindowOptions windowOptions = WindowOptions(
+      center: true,
+      title: t.appName,
+      size: Size(1000, 700),
+      minimumSize: Size(700, 500),
+    );
     windowManager.waitUntilReadyToShow(windowOptions, () async {
       await windowManager.show();
       await windowManager.focus();
