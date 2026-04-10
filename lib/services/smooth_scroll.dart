2154fdc266eabfcdcbdec379651a06caf1ead260
FlamingWater35 <flamingwater69420@gmail.com>
2026-04-10 23:57:02 +0300

    Revert "Update smooth scrolling logic"
    This reverts commit 6837a2d7aeadf8f1610cddbe71783cd46e1dcb27.

--- lib/services/smooth_scroll.dart
+++ lib/services/smooth_scroll.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class SmoothScrollController extends ScrollController {
@@ -41,16 +38,6 @@ class SmoothScrollPosition extends ScrollPositionWithSingleContext {
 
   @override
   void pointerScroll(double delta) {
-    if (!kIsWeb && Platform.isAndroid) {
-      super.pointerScroll(delta);
-      return;
-    }
-
-    if (delta.abs() < 20) {
-      super.pointerScroll(delta);
-      return;
-    }
-
     if (delta == 0.0) return;
 
     if (activity is! DrivenScrollActivity) {
